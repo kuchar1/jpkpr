92f6f24d589d408f642b51a08ab0f4a81f3619fd
4 <4@10.12.9.109>
2014-08-12 13:25:32 +0200

    12-08-2014 13:25

--- src/MyOwnPaint/FrameForPaint.java
+++ src/MyOwnPaint/FrameForPaint.java
@@ -1,25 +1,12 @@
 package MyOwnPaint;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.*;
 
-import AppletPaint.Paint;
+
 
 public class FrameForPaint extends JFrame implements ActionListener{
 
